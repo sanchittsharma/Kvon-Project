2800d09e0c0cef9803b2518e69e5245ea65bcc30
sanchittsharma <142312010+sanchittsharma@users.noreply.github.com>
2025-05-14 12:41:38 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,31 @@
 const express=require("express")
 const mongoose=require("mongoose")
-const {registrictToUser}=require("../middlewares/auth")
+const {registrictToLoginUser}=require("../middlewares/auth")
 const passport=require("passport")
-const { handleGoogleAuth } = require("../controllers/user")
+// const { handleGoogleAuth } = require("../controllers/user")
 const router=express.Router()
+const {handleChangePassword,handleForgotPassword,handleGoogleAuth,handleResetPassword,handleUserRegister,handleuserLogin}=require("../controllers/user")
 
 
-
-router.post("/changepass",registrictToUser,handleChangePassword)
-router.post("/forgotPassword",handleForgotPassword)
+router.get("/login",async(req,res)=>{
+    return res.render("login")
+})
+router.get("/",async(req,res)=>{
+    return res.render("login")
+})
+router.get("/signup",async(req,res)=>{
+    return res.render("signup")
+})
+router.get("/forgot-password",async(req,res)=>{
+    return res.render("forgot-password",{message:null})
+})
+router.post("/login",handleuserLogin)
+router.post("/signup",handleUserRegister)
+router.post("/changepass",registrictToLoginUser,handleChangePassword)
+router.post("/forgot-Password",handleForgotPassword)
 router.patch("/resetpassword/:token",handleResetPassword)
 
-router.get('/google',
+router.get('/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] })
   );
   
