96491022198f87bcb97016d80e08150e82c465d9
sanchittsharma <142312010+sanchittsharma@users.noreply.github.com>
2025-05-15 11:18:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const expressSession=require("express-session")
 const cookieparser=require("cookie-parser")
 const passport=require("passport")
 const userroute=require("./routes/user")
-const port=5000
+const port=1000
 connectToMongodb("mongodb://127.0.0.1:27017/CourseProject")
 // app.use(cookieparser())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.set("view engine" ,"ejs")
 app.use(cookieparser())
-app.use(passport.initialize())
 app.use(expressSession({
     secret: 'Sanchit@123$', 
     resave: false,
     saveUninitialized: true
   }));
+app.use(passport.initialize())
 app.use(passport.session())
 app.set("views",path.resolve("./views"))
 app.use("/",userroute)
